5a395aff5587d98ff684aa52fdc4b76010d8f355
Ida Akiwumi <618307+IdaAkiwumi@users.noreply.github.com>
2021-05-13 00:15:55 -0400

    v

--- server.js
+++ server.js
@@ -1,25 +1,45 @@
+ function getNoteId() {
+        let noteObject = getExistingNotes()
+        if (!noteObject) {
+            return 1
+        }
 
-function getNoteId()
-{
-    let noteObject = getExistingNotes()
-    if (!noteObject){
-        return 1
+
+        const keysArray = Object.keys(noteObject);
+        const numberKeys = keysArray.map((key) => Number(key))
+        return Math.max(...numberKeys) + 1
+
     }
 
-
-const keysArray = Object.keys(noteObject)
-const numberKeys = keysArray.map((key) => Number(key))
-return Math.max(...numberKeys) + 1
-
-}
-
-function getExistingNotes(){
-    let notes= localStorage.getItem("notes")
-    if (!notes){
-        return null
+    function getExistingNotes() {
+        let notes = localStorage.getItem("notes")
+        if (!notes) {
+            return null
+        }
+        return JSON.parse(notes)
     }
-    return JSON.parse(notes)
-}
+    
 
 
 
+
+
+
+/*
+const http = require('http');
+
+
+const localStorage = http.createServer(function(req,res) {
+
+res.writeHead(200, {'Content-Type': 'text/html'});
+
+res.end(
+
+'notesList'
+);
+
+
+});
+
+
+localStorage.listen(5500, 'localhost');*/
